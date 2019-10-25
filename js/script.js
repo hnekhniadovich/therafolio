5408dbf7dad258d78801d34128e38e1cf139376d
Hanna Nekhniadovich <hanna@nextguest.com>
2019-10-25 19:04:31 -0400

    replace owl-carousel with slick-carousel

--- js/script.js
+++ js/script.js
@@ -1,41 +1,13 @@
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     pagination:false,
-//     autoplay: 1000,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:1
-//         },
-//         700: {
-//             items:1
-//         },
-//         1000:{
-//             items:1
-//         }
-//     }
-// })
-
-$(document).ready(function() {
- 
-    $("#slider").owlCarousel({
-   
-        navigation : true, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-   
-        // "singleItem:true" is a shortcut for:
-        // items : 1, 
-        // itemsDesktop : false,
-        // itemsDesktopSmall : false,
-        // itemsTablet: false,
-        // itemsMobile : false
-   
+$(document).ready(function(){
+    $('.header__slider').slick({
+        autoplay: true,
+        arrows: true,
+        autoplaySpeed: 6000,
+        infinite: true,
+        speed: 800,
+        fade: true,
+        cssEase: 'linear'
     });
-   
-  });
+});
+
+
